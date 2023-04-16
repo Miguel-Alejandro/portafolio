d0cd74f5c6f4cbbe339a23ea31669ee01f15e2a5
miguel <miguel.arellano@jaak.ai>
2023-04-15 18:22:06 -0600

    feat: the angular cdk, fontawesome was installed and the variables of the colors were created

---
+++ src/app/classes/icons/icons.ts
@@ -0,0 +1,9 @@
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+
+export class IconClass {
+    public menuIcon = faBars;
+    public closeIcon = faXmark;
+
+    constructor(){}
+
+}
