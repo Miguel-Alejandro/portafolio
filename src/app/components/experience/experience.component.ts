34ef514ae78ec727d407b1f13a02d3f0af70a298
miguel <miguel.arellano@jaak.ai>
2023-04-16 15:53:52 -0600

    feat: the experience section was created

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class ExperienceComponent {
 
+  public works = [
+    {
+      name: 'Digital Class',
+      duration: '2020 - 2021',
+      functions: [
+        {
+          'functionName': 'Desarrollar aplicaciones web en python y django'
+        },
+        {
+          'functionName': 'Crear diseño para las aplicaciones web'
+        }
+      ]
+
+    },
+    {
+      name: 'Jaak',
+      duration: '2021 - 2023',
+      functions: [
+        {
+          'functionName': 'Desarrollar el frontend de los dashboards'
+        },
+        {
+          'functionName': 'Consumir apis que contenían inteligencia artificial'
+        },
+        {
+          'functionName': 'Crear pruebas E2E'
+        }
+      ]
+
+    }
+  ];
+
 }
