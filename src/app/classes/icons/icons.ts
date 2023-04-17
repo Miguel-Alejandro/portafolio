f30b579c0ba7a4a7a9c4bca58f57dd81fd49e9c7
miguel <miguel.arellano@jaak.ai>
2023-04-17 00:18:56 -0600

    fix: the about me session was designed and the contact information was added

--- src/app/classes/icons/icons.ts
+++ src/app/classes/icons/icons.ts
@@ -1,8 +1,12 @@
-import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faXmark, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 export class IconClass {
     public menuIcon = faBars;
     public closeIcon = faXmark;
+    public githubIcon = faGithub;
+    public linkedinIcon = faLinkedin;
+    public emailIcon = faEnvelope;
 
     constructor(){}
 
